d940dadda98886066b770dceec4617d889dc2a1f
Phy Poh <phypoh@gmail.com>
2023-10-23 01:54:17 +0800

    Remove delay from text popup

--- LyreCounter.lua
+++ LyreCounter.lua
@@ -7,9 +7,9 @@ if ModUtil ~= nil then
     --Zeus Duo extra function
 	ModUtil.Path.Wrap("UseLyre",
 		function(baseFunc, usee, args)
-			baseFunc(usee, args)
-            thread(InCombatTextArgs,
+			thread(InCombatTextArgs,
 			{ TargetId = CurrentRun.Hero.ObjectId, Text = "LyreCounterText", Duration = 1, LuaKey = "TempTextData",
-				LuaValue = { Amount = GameState.ItemInteractions.HouseLyre01 } })
+				LuaValue = { Amount = GameState.ItemInteractions.HouseLyre01 +1 } })
+			baseFunc(usee, args)
 		end)
 end 
